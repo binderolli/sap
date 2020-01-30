4c467086c74ac178a1c12b9b36031253795def0d
binderolli <44654035+binderolli@users.noreply.github.com>
2020-01-30 21:00:57 +0100

    Update masternodeupdate.sh

--- masternodeupdate.sh
+++ masternodeupdate.sh
@@ -4,7 +4,7 @@ echo Update START
 rm -r sapd
 rm -r sap-cli
 wget https://github.com/sappcoin-com/SAPP/releases/download/v1.2.0/SAPP-Linux1604.zip
-unzip  SAPP-Linux-1604-Daemon.zip
+unzip SAPP-Linux-1604-Daemon.zip
 chmod 777 *
 ./sap-getinfo
 echo Update finished
